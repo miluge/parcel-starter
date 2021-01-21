881f7dc9454ccbc0b9991ebd4433ab3af16fde3a
miluge <gllmblndl@gmail.com>
2021-01-21 23:45:38 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('Howdy Stranger!')
+
+import Rellax from 'rellax';
+
+let rellax = new Rellax('.rellax');
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontSize: {
+                'big': '8.138vw',
+                'heading': '2.1875vw',
+                'project': '18vh'
+            },
+            colors: {
+                'space-cadet': '#2E294E',
+                'eggshell': '#F1E9DA',
+            },
+            fontFamily: {
+                'inter': ['"Inter Bold"', 'serif'],
+                'garamond': ['"EB Garamond SemiBold"', 'serif'],
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+}
